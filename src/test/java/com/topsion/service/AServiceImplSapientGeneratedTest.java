5bae6a2e7b8e311a7af8959b232b0f9cba05bb64
topsion <wensir_top@163.com>
2023-08-24 23:38:06 +0800

    Add sapient ai for ut

---
+++ src/test/java/com/topsion/service/AServiceImplSapientGeneratedTest.java
@@ -0,0 +1,17 @@
+package com.topsion.service;
+
+import org.junit.jupiter.api.Test;
+
+class AServiceImplSapientGeneratedTest {
+
+    //Sapient generated method id: ${f6af188f-5d9d-3244-83f7-cfc8c16873f9}
+    @Test()
+    void sayHelloTest() {
+        /*
+         * TODO: Help needed! Please adjust the input/test parameter values manually to satisfy the requirements of the given test scenario.
+         *  The test code, including the assertion statements, has been successfully generated.
+         */
+        AServiceImpl target = new AServiceImpl("name1", 0);
+        target.sayHello();
+    }
+}
