e7eae5eb848ebbca2d0140f6d91e88b823e8c5d9
topsion <wensir_top@163.com>
2023-08-31 19:33:30 +0800

    Rename ut name

--- src/test/java/com/topsion/service/AServiceImplSapientGeneratedTest.java
+++ src/test/java/com/topsion/service/AServiceImplSapientGeneratedTest.java
@@ -6,7 +6,7 @@ class AServiceImplSapientGeneratedTest {
 
     //Sapient generated method id: ${f6af188f-5d9d-3244-83f7-cfc8c16873f9}
     @Test()
-    void sayHelloTest() {
+    void should_say_hello_when_IoC_create_bean_successful() {
         /*
          * TODO: Help needed! Please adjust the input/test parameter values manually to satisfy the requirements of the given test scenario.
          *  The test code, including the assertion statements, has been successfully generated.
